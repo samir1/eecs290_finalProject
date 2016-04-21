5ab6ff832f34a4c49750ae8102b76e2de2a4c2ce
Samir <samir1@brandeis.edu>
2016-04-21 15:16:20 -0400

    add time for health

--- zombiegame/Assets/Scripts/PlayerCharacter.cs
+++ zombiegame/Assets/Scripts/PlayerCharacter.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerCharacter : MonoBehaviour {
 	private int _health;
 
 	void Start() {
-		_health = 5;
+		_health = 100;
 	}
 
 	public void Hurt(int damage) {
 		_health -= damage;
 		Debug.Log("Health: " + _health);
 	}
+
+	public void Update() {
+		Debug.Log (Time.time);
+	}
+
 }
