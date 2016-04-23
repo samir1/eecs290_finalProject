c370aabda6d7ad80944d3fe0ff120d8ca939b36c
ShingJan <yuanj.shi@hotmail.com>
2016-04-23 17:36:24 -0400

    Safe House

---
+++ zombiegame/Assets/Scripts/SafeHouse.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine
+using System.Collections;
+
+public class SafeHouse : MonoBehaviour {
+
+
+	public Transform movingHouse;
+	public Transform position1;
+	public Transform position2;
+	public Transform position3;
+	public Transform position4;
+
+	public Vector3 newPosition;
+	public string currentState;
+	public float smooth;
+	public float resetTime;
+
+ 
+	void Start () {
+
+	}
+
+	void FixedUpdate () {
+		movingHouse.position = Vector3.Lerp (movingHouse.position, newPosition, smooth * Time.delatTime);
+	}
+
+
+}
+
+
