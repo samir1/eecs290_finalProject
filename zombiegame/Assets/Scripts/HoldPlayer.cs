d9a976639aa037c7223880c8c333e49595eebe49
ShingJan <yuanj.shi@hotmail.com>
2016-04-23 18:01:21 -0400

    Hold Player

---
+++ zombiegame/Assets/Scripts/HoldPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class HoldPlayer : MonoBehaviour{
+	void OnTriggerEnter(Collider col){
+		col.transform.parent = gameObject.transform;
+	}
+
+	void OnTriggerExit(Collider col){
+		col.transform.parent = null;
+	}
+}
+
+
