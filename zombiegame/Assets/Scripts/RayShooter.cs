80378005948b602618a0f5af9ea021f17fa56d84
Audaris Blades <awb72@case.edu>
2016-04-25 04:10:13 -0400

    WIP(Bat transform and hit detection)

--- zombiegame/Assets/Scripts/RayShooter.cs
+++ zombiegame/Assets/Scripts/RayShooter.cs
@@ -8,15 +8,16 @@ public class RayShooter : MonoBehaviour {
 	private Camera _camera;
 	private GameObject _player;
 	private GameObject _bat;
-
 	public Texture reticle;
-
+	Vector3 originalPos;
+	Quaternion originalRot;
 	void Start() {
 		_bat = GameObject.Find("BaseballBat");
 		_camera = GetComponent<Camera>();
 		_player = GameObject.Find("Player");
 		_player.layer = LayerMask.NameToLayer("Ignore Raycast");
-
+		originalPos = _bat.transform.localPosition;
+		originalRot = _bat.transform.localRotation;
 		//Cursor.lockState = CursorLockMode.Locked;
 		//Cursor.visible = false;
 	}
@@ -51,10 +52,15 @@ public class RayShooter : MonoBehaviour {
 
 		if (Input.GetMouseButton (0))
 			batAnimation ();
+		else {
+			_bat.transform.localPosition = originalPos;
+			_bat.transform.localRotation = originalRot;
+		}
 	}
-
+		
 	private void batAnimation(){
-		_bat.transform.Translate (Vector3.right * Time.deltaTime);
+		_bat.transform.Translate (new Vector3(1f, -.3f, -.4f) * Time.deltaTime);
+		_bat.transform.Rotate (new Vector3(1f,.3f, .4f), 30 * Time.deltaTime);
 	}
 
 	private IEnumerator SphereIndicator(Vector3 pos) {
