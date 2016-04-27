3b01f176abfab16386e45ba0df1287894dd1a4f1
Audaris Blades <awb72@case.edu>
2016-04-27 02:53:26 -0400

    Added Gun and projectiles, Fixed deleted onhit sound........
    Still need to add sound effects and textures and have gun register

---
+++ zombiegame/Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+	public float delayTime = 8;
+	private AudioSource source;
+	public AudioClip onShoot;//attaches audio component to player
+	private float counter = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.Mouse0) && counter > delayTime) {
+			Instantiate (bullet, transform.position, transform.rotation);
+			source.PlayOneShot (onShoot);
+			counter = 0;
+		}
+		counter += 1 * Time.deltaTime;
+
+	}
+}
